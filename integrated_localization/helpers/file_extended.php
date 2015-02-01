0b2022edba3603c75ae83ef683c83b9e69402963
Michele Locati <michele@locati.it>
2015-02-01 21:00:20 +0100

    Add getUploadedFile method

--- integrated_localization/helpers/file_extended.php
+++ integrated_localization/helpers/file_extended.php
@@ -39,4 +39,43 @@ class FileExtendedHelper
 
         return $result;
     }
+    /**
+     * @param string $fieldName
+     * @param bool $allowEmpty
+     * @throws Exception
+     * @return array|null
+     */
+    public function getUploadedFile($fieldName, $allowEmpty = false)
+    {
+        if (!(isset($_FILES) && isset($_FILES[$fieldName]) && is_array($_FILES[$fieldName]))) {
+            return;
+        }
+        $file = $_FILES[$fieldName];
+        switch ($file['error']) {
+            case UPLOAD_ERR_OK:
+                break;
+            case UPLOAD_ERR_INI_SIZE:
+                throw new Exception(t('The uploaded file exceeds the upload_max_filesize directive in php.ini.'));
+            case UPLOAD_ERR_FORM_SIZE:
+                throw new Exception(t('The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.'));
+            case UPLOAD_ERR_PARTIAL:
+                throw new Exception(t('The uploaded file was only partially uploaded.'));
+            case UPLOAD_ERR_NO_FILE:
+                throw new Exception(t('No file was uploaded.'));
+            case UPLOAD_ERR_NO_TMP_DIR:
+                throw new Exception(t('Missing a temporary folder.'));
+            case UPLOAD_ERR_CANT_WRITE:
+                throw new Exception(t('Failed to write file to disk.'));
+            case UPLOAD_ERR_EXTENSION:
+                throw new Exception(t('A PHP extension stopped the file upload.'));
+            default:
+                throw new Exception(t('Unknown error occurred during file upload (%s).', $file['error']));
+        }
+        unset($file['error']);
+        if ((!$allowEmpty) && ($file['size'] <= 0)) {
+            throw new Exception(t('The uploaded file is empty.'));
+        }
+
+        return $file;
+    }
 }
