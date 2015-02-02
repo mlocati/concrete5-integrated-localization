3ba18f22579c383abc767b39013af5e698458238
Michele Locati <michele@locati.it>
2015-02-02 10:34:10 +0100

    Formatting

--- integrated_localization/single_pages/dashboard/integrated_localization/locales.php
+++ integrated_localization/single_pages/dashboard/integrated_localization/locales.php
@@ -118,30 +118,24 @@ if ($editing) {
                     <table border="0" cellspacing="0" cellpadding="0" class="ccm-results-list">
                         <thead>
                             <tr>
-                                <th><?php echo t('Name');
-                    ?></th>
-                                <th><?php echo t('Identifier');
-                    ?></th>
+                                <th><?php echo t('Name'); ?></th>
+                                <th><?php echo t('Identifier'); ?></th>
                                 <?php
                                 if (!$approved) {
                                     ?>
-                                    <th><?php echo t('Requested by');
-                                    ?></th>
-                                    <th><?php echo t('Requested on');
-                                    ?></th>
+                                    <th><?php echo t('Requested by'); ?></th>
+                                    <th><?php echo t('Requested on'); ?></th>
                                     <th style="width: 100px"></th>
                                     <?php
-
                                 }
-                    ?>
+                            ?>
                             </tr>
                         </thead>
                         <tbody>
                     <?php
                     $found = true;
                 }
-                ?><tr<?php echo $locale->getIsSource() ? ' style="background-color: #fafcba"' : '';
-                ?>>
+                ?><tr<?php echo $locale->getIsSource() ? ' style="background-color: #fafcba"' : ''; ?>>
                     <td><a <?php
                         if ($locale->getIsSource()) {
                             ?> href="javascript:void(0)" disabled="disabled" onclick="<?php echo $th->specialchars('alert('.json_encode(t("This is the source locale and can't be modified")).')'); ?>"<?php
@@ -168,8 +162,7 @@ if ($editing) {
                                 echo t('Nobody');
                             }
                         ?></td>
-                        <td><?php echo $th->specialchars($locale->getRequestedOn());
-                        ?></td>
+                        <td><?php echo $th->specialchars($locale->getRequestedOn()); ?></td>
                         <td style="white-space: nowrap">
                             <?php echo $cih->button(t('Deny'), View::url('/dashboard/integrated_localization/locales', 'delete', $locale->getID()), '', 'small danger', array('onclick' => $th->specialchars('return confirm('.json_encode(t('Are you sure?')).')'))); ?>
                             <?php echo $cih->button(t('Approve'), View::url('/dashboard/integrated_localization/locales', 'approve', $locale->getID()), '', 'small success', array('onclick' => $th->specialchars('return confirm('.json_encode(t('Are you sure?')).')'))); ?>
