196b0d86c8954dd27d2c307427976fb51e6b2ef2
Michele Locati <michele@locati.it>
2015-02-04 12:14:29 +0100

    More work on the translate page

--- integrated_localization/controllers/integrated_localization/translate.php
+++ integrated_localization/controllers/integrated_localization/translate.php
@@ -2,39 +2,157 @@
 
 class IntegratedLocalizationTranslateController extends Controller
 {
+    private function addTabs($section)
+    {
+        $hh = Loader::helper('html');
+        /* @var $hh HtmlHelper */
+        $this->addHeaderItem($hh->css('tabs.css', 'integrated_localization'));
+        $this->addHeaderItem($hh->css('progress.css', 'integrated_localization'));
+        $this->set('tab', $section);
+    }
     public function view()
+    {
+        $this->redirect('/integrated_localization/translate/core_development');
+    }
+    public function core_development($selectedLocale = '*auto*')
+    {
+        $this->addTabs(__FUNCTION__);
+        $this->set('locales', $this->getLocales($selectedLocale));
+        $this->loadCoreVersions(true);
+    }
+    public function core_releases($selectedLocale = '*auto*')
+    {
+        $this->addTabs(__FUNCTION__);
+        $this->set('locales', $this->getLocales($selectedLocale));
+        $this->loadCoreVersions(false);
+    }
+    public function packages($selectedLocale = '*auto*')
+    {
+        $this->addTabs(__FUNCTION__);
+        $this->set('locales', $this->getLocales($selectedLocale));
+    }
+    private function loadCoreVersions($dev)
     {
         $db = Loader::db();
         /* @var $db ADODB_mysql */
-        $devVersions = array();
-        $releaseVersions = array();
-        foreach ($db->GetCol("SELECT DISTINCT itpVersion FROM IntegratedTranslatablePlaces WHERE (itpPackage = '-')") as $v) {
-            if (strpos($v, 'dev-') === 0) {
-                $devVersions[$v] = substr($v, 4);
+        $list = array();
+        foreach ($db->GetCol("SELECT DISTINCT itpVersion FROM IntegratedTranslatablePlaces WHERE (itpPackage = '-') AND (itpVersion ".($dev ? '' : 'NOT ')."LIKE 'dev-%')") as $v) {
+            if ($dev) {
+                $list[$v] = substr($v, 4);
             } else {
-                $releaseVersions[$v] = $v;
+                $list[$v] = $v;
             }
         }
-        uasort($devVersions, 'version_compare');
-        $devVersions = array_reverse($devVersions, true);
-        uasort($releaseVersions, 'version_compare');
-        $releaseVersions = array_reverse($releaseVersions, true);
-        $coreVersions = array();
-        $nDevVersions = count($devVersions);
-        $iDevVersion = 0;
-        foreach (array_keys(array_merge($devVersions, $releaseVersions)) as $v) {
-            if (strpos($v, 'dev-') === 0) {
-                $iDevVersion++;
-                if (($iDevVersion > 1) && ($iDevVersion === $nDevVersions)) {
-                    $name = t('Development version (up to the %s series)', substr($v, 4));
+        uasort($list, 'version_compare');
+        $list = array_reverse($list, true);
+        $versions = array();
+        $n = count($list);
+        $i = 0;
+        foreach ($list as $real => $shown) {
+            if ($dev) {
+                $i++;
+                if (($i > 1) && ($i === $n)) {
+                    $name = t('Development version (up to the %s series)', $shown);
                 } else {
-                    $name = t('Development version (from the %s series)', substr($v, 4));
+                    $name = t('Development version (from the %s series)', $shown);
                 }
             } else {
-                $name = t('Release %s', $v);
+                $name = t('concrete5 %s', $shown);
             }
-            $coreVersions[$v] = $name;
+            $versions[$real] = $name;
         }
-        $this->set('coreVersions', $coreVersions);
+        $this->set('versions', $versions);
+    }
+    private function getLocales($selectedLocale = '*auto*')
+    {
+        Loader::model('integrated_locale', 'integrated_localization');
+        $result = array(
+            'all' => IntegratedLocale::getList(),
+            'mine' => array(),
+            'notMine' => array(),
+        );
+        if (User::isLoggedIn()) {
+            $th = Loader::helper('translators', 'integrated_localization');
+            /* @var $th TranslatorsHelper */
+            $me = new User();
+            foreach ($result['all'] as $locale) {
+                if ($th->getUserAccess($me, $locale) >= TranslatorAccess::LOCALE_TRANSLATOR) {
+                    $result['mine'][] = $locale;
+                } else {
+                    $result['notMine'][] = $locale;
+                }
+            }
+        }
+        $locale = null;
+        if (is_object($selectedLocale)) {
+            $locale = $selectedLocale;
+        } elseif (is_string($selectedLocale) && ($selectedLocale !== '')) {
+            if ($selectedLocale === '*auto*') {
+                if (count($result['mine']) > 0) {
+                    $locale = $result['mine'][0];
+                }
+            } else {
+                foreach ($result['all'] as $l) {
+                    if ($l->getID() === $selectedLocale) {
+                        $locale = $l;
+                        break;
+                    }
+                }
+                if (!isset($locale)) {
+                    throw new Exception(t("Unable to find the locale with id '%s'", $selectedLocale));
+                }
+            }
+        }
+        $result['selected'] = $locale;
+
+        return $result;
+    }
+    public function getVersionStats($package, $version, IntegratedLocale $locale)
+    {
+        $result = array(
+            'total' => 0,
+            'translated' => 0,
+            'progress' => 0,
+        );
+        $db = Loader::db();
+        /* @var $db ADODB_mysql */
+        $n = $db->GetOne(
+            '
+                SELECT COUNT(itpTranslatable)
+                FROM IntegratedTranslatablePlaces
+                WHERE (itpPackage = ?) AND (itpVersion = ?)
+            ',
+            array($package, $version)
+        );
+        if ($n) {
+            $result['total'] = intval($n);
+        }
+        if ($result['total'] > 0) {
+            $n = $db->GetOne(
+                '
+                    SELECT COUNT(*)
+                    FROM IntegratedTranslatablePlaces
+                    INNER JOIN IntegratedTranslations ON IntegratedTranslatablePlaces.itpTranslatable = IntegratedTranslations.itTranslatable
+                    WHERE (IntegratedTranslatablePlaces.itpPackage = ?) AND (IntegratedTranslatablePlaces.itpVersion = ?) AND (IntegratedTranslations.itLocale = ?)
+                ',
+                array($package, $version, $locale->getID())
+            );
+            if ($n) {
+                $result['translated'] = intval($n);
+                if ($result['translated'] === $result['total']) {
+                    $result['progress'] = 100;
+                } else {
+                    $p = round($result['translated'] * 100 / $result['total']);
+                    if ($p < 1) {
+                        $p = 1;
+                    } elseif ($p == 100) {
+                        $p = 99;
+                    }
+                    $result['progress'] = $p;
+                }
+            }
+        }
+
+        return $result;
     }
 }
--- integrated_localization/single_pages/integrated_localization/translate.php
+++ integrated_localization/single_pages/integrated_localization/translate.php
@@ -5,12 +5,83 @@
 $th = Loader::helper('text');
 /* @var $th TextHelper */
 
-$fh = Loader::helper('form');
-/* @var $fh FormHelper */
+$selectedLocaleID = isset($locales['selected']) ? $locales['selected']->getID() : '';
+?>
+<div>
+    <?php echo t('Selected language:'); ?> 
+    <select onchange="<?php echo $th->specialchars('window.location.href = '.json_encode($this->action($tab, 'LOCALE_PLACEHOLDER')).'.replace("LOCALE_PLACEHOLDER", this.value);'); ?>"><?php
+        if(!isset($locales['selected'])) {
+            ?><option value="" selected="selected"><?php echo t('*** Please select'); ?></option><?php
+        }
+        if ((count($locales['mine']) > 0) && (count($locales['notMine']) > 0)) {
+            ?>
+            <optgroup label="<?php echo t('My languages'); ?>">
+                <?php
+                foreach($locales['mine'] as $l) {
+                    ?><option value="<?php echo $th->specialchars(rawurlencode($l->getID())); ?>"<?php echo ($locales['selected'] === $l) ? ' selected="selected"' : ''; ?>><?php echo $th->specialchars($l->getName()); ?></option><?php
+                }
+                ?>
+            </optgroup>
+            <optgroup label="<?php echo t('Other languages'); ?>">
+                <?php
+                foreach($locales['notMine'] as $l) {
+                    ?><option value="<?php echo $th->specialchars(rawurlencode($l->getID())); ?>"<?php echo ($locales['selected'] === $l) ? ' selected="selected"' : ''; ?>><?php echo $th->specialchars($l->getName()); ?></option><?php
+                }
+                ?>
+            </optgroup>
+            <?php
+        }
+        else {
+            foreach($locales['all'] as $l) {
+                ?><option value="<?php echo $th->specialchars(rawurlencode($l->getID())); ?>"<?php echo ($locales['selected'] === $l) ? ' selected="selected"' : ''; ?>><?php echo $th->specialchars($l->getName()); ?></option><?php
+            }
+        }
+    ?></select>
+</div>
 
-?><h2><?php echo t('Core Translations'); ?></h2>
-
+<ul class="integrated_localization-tabs">
+  <li<?php echo ($tab === 'core_development') ? ' class="active"' : ''; ?>><a href="<?php echo $this->action('core_development', $selectedLocaleID); ?>"><?php echo t('Core versions - Development'); ?></a></li>
+  <li<?php echo ($tab === 'core_releases') ? ' class="active"' : ''; ?>><a href="<?php echo $this->action('core_releases', $selectedLocaleID); ?>"><?php echo t('Core versions - Releases'); ?></a></li>
+  <li<?php echo ($tab === 'packages') ? ' class="active"' : ''; ?>><a href="<?php echo $this->action('packages', $selectedLocaleID); ?>"><?php echo t('Packages'); ?></a></li>
+</ul>
 <?php
-foreach ($coreVersions as $$version => $name) {
-	?><p><?php echo $name; ?></p><?php
+switch($tab) {
+    case 'core_development':
+    case 'core_releases':
+        
+        ?>
+        <table class="table table-striped table-condensed table-bordered table-hover">
+            <thead><tr>
+                <th><?php echo t('Version'); ?></th>
+                <th><?php echo t('Details'); ?></th>
+                <th><?php echo t('Progress'); ?></th>
+            </tr></thead>
+            <tbody><?php
+                foreach ($versions as $version => $name) {
+                    ?>
+                    <tr>
+                        <th><?php echo $th->specialchars($name); ?></th>
+                        <?php
+                        if(isset($locales['selected'])) {
+                            $stats = $this->controller->getVersionStats('-', $version, $locales['selected']);
+                            ?>
+                            <td><?php echo t('%s out of %s', $stats['translated'], $stats['total']); ?></td>
+                            <td style="width: 120px"><div class="integrated_localization-progress integrated_localization-progress-<?php echo floor($stats['progress'] / 10); ?>" title="<?php echo t('%s %%', $stats['progress']); ?>">
+                                <span style="width: <?php echo $stats['progress']; ?>%" ><?php echo t('%s %%', $stats['progress']); ?></span>
+                            </div></td>
+                            <?php
+                        } else {
+                            ?><td colspan="2"><?php echo t('Select a language to see the details'); ?></td><?php
+                        }
+                        ?>
+                    </tr>
+                    <?php
+                }
+                ?>       
+            </tbody>
+        </table>
+        <?php
+        break;
+    case 'packages':
+        break;
 }
